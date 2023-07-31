d5b0e9a15e5cf09e7592df51183bbe972a060c81
Jieun Kim <jjinttol1108@gmail.com>
2023-07-31 20:42:11 +0900

    김지은 31일 문제풀이

--- 230731.js
+++ 230731.js
@@ -10,10 +10,11 @@
     };
     c(a, b);
 
-    {
-    // 이름 :
-    // 예측한 정답은 여기에 기입~~~
-    }
+        // ... = 배열을 풀게!
+        // [...a] = [...[1,2,3]] = [1,2,3]
+        // ...[...b] = ...[...["you", "are"]] = ...["you", "are"] = "you", "are"
+        // [[1,2,3], "you", "are"]
+    
 
     //2번 문제 - 민아
     // 아래 numbers에서 Math.max, Math.min, apply를 이용하여 최대값, 최소값을
@@ -21,18 +22,41 @@
     var numbers = [5, 6, 2, 3, 7];
 
     // 여기에 코드 적기~
-
-    console.log(max);
-    console.log(min);
+    console.log(Math.max(...numbers));
+    // console.log(Math.max.apply(null, numbers));
+    console.log(Math.min(...numbers));
+    // console.log(Math.min.apply(null, numbers));
 }
 
 {
     // 대훈
     //1. 매개변수 받지 않고 두개의 인자의 합을 구하는 함수 만들어보기
+
+    // 매개변수 사용하지 않으면 arguments를 선언해서 사용해야
+    let sum = function () { 
+        let a = arguments[0];
+        let b = arguments[1];
+        console.log(a + b);
+    };
+    sum(1, 2);
+
+    let sum1 = function (a, b) { // 매개변수 사용시!!
+        console.log(a + b);
+    };
+    sum1(3, 4);
+
     
     //2. 기존 매개변수는 그대로 두고 인자를 추가해서 함수를 출력할때 덧셈의 합이 10이 되도록 해보기
+    let sum2 = function (x, y, ...rest) {
+        console.log(rest[0] + rest[1]+ rest[2])
+    };
+    sum2(1, 2, 3, 2, 5)
 
     //3. 숫자배열, 문자배열 각각 만들어서 하나의 1차배열로 한번에 출력하기
+    let arr = [1, 2, 3, 4, 5];
+    let str = ['a', 'b', 'c', 'd']; 
+
+    console.log([...arr, ...str]);
 }
 
 {
@@ -40,13 +64,27 @@
     let arr = [10, 60, 30, 20, 24, 15];
     // function()과 매개 변수를 이용한 오름차순 정렬
 
+    arr.sort(function (a,b)  {
+        return a - b;
+    });
+    console.log(arr);
+
     // arr 배열에 35값 추가, 10 제거한 후 출력 => 메소드 이용
+    arr.shift();
+    arr.push(35);
+    console.log(arr);
 
     // arrow function을 이용한 오름차순 정렬
+    arr.sort((a,b) => a - b);
+    console.log(arr);
 
     // arrow function을 이용한 내림차순 정렬 후 출력
+    arr.sort((a,b) => b - a);
+    console.log(arr);
 
     // splice를 이용하여 35를 제거하고 100,101을 추가하기
+    arr.splice(1, 1, 100, 101)
+    console.log(arr);
 }
 
 {
@@ -58,14 +96,16 @@
         console.log(a + b);
     }
     test(undefined, undefined);
-    }
+    } // undefined이면 기본값이 그대로 들어가면 됨! 
 
     console.log("------문제2------");
     {
     // 아래의 배열 값을 내림차순으로 정렬하시오.
     // .sort(), arrow function 사용하기!
 
-    let nums = [10, 20, 39, 68, 92, 45, 100];
+        let nums = [10, 20, 39, 68, 92, 45, 100];
+        nums.sort((a, b) => b-a);
+        console.log(nums);
     }
 }
 
@@ -75,10 +115,13 @@
 
     let nums = [1, 2, 45, 60, 78, 27];
     let nums1 = [3, 5, 8, ...nums];
+    // 3, 5, 8, 1, 2, 45, 60, 78, 27_ nums를 풀었음! 
 
     function print([x, y, ...aa]) {
+    // aa: 8, 1, 2, 45, 60, 78, 27
     console.log(x, y, aa[2], aa.length, arguments.length, arguments[2]);
     }
 
     print(nums1);
+    // 3, 5, 2, 7,arguments !!! 괄호 안에 들어가는 값이 몇개?! "nums1"1개! = 배열로 묶여서 배열1개! 들어감 ! => length 1개! 
 }
